dcddf5a956473e9f0336e6abab8860f2631b38d8
Hari Furniture <harifurniturekhm@gmail.com>
2026-02-22 16:57:55 +0530

    chore: fix CORS for PATCH requests

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
         res.setHeader("Access-Control-Allow-Origin", "https://khm-frontend-xi.vercel.app");
     }
 
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
 
